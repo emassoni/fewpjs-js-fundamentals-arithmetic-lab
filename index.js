f9f9e562bf4b739c8fbd51b0f135a22a3b8315a2
Eric <eric.d.massoni@gmail.com>
2021-04-07 16:23:33 -0400

    completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2= 2
+let multiply= num1 * num2
+
+let random = Math.floor(Math.random()*5+1)
+
+let num3 = 20;
+let num4 = 16;
+let mod = num3 % num4;
+
+let max = Math.max(4, 8, 18, 20);
 
 // Write your code here
